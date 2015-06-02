547419014df79fca555ba8a38a82fca247a3ad8a
David.Schroh <dschroh@uncharted.software>
2015-06-02 09:24:04 -0400

    trim out unneeded content from vagrant provisioning

--- provision.sh
+++ provision.sh
@@ -22,72 +22,6 @@ sudo sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config /etc/s
 sudo pip install --upgrade pip 
 sudo pip install --upgrade virtualenv
 
-# IPython
-#   Note:   ipython notebook --ip='*'
-sudo pip install ipython[all]
-
-# Pandas
-#sudo pip install pandas
-
-# Newspaper: Scraping, Extraction and NLP 
-#cd /vagrant
-#sudo pip install newspaper
-#sudo curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python
-
-# Goose: Content Extrction
-#cd /vagrant
-#sudo mkvirtualenv --no-site-packages goose
-#git clone https://github.com/grangier/python-goose.git
-#cd python-goose
-#sudo pip install -r requirements.txt
-#sudo python setup.py install
-
-# Feedparser
-#sudo pip install feedparser
-
-# Work with CSV files on the command line
-#sudo pip install csvkit
-
-# Work with Excel files
-#sudo pip install xlsx2csv
-
-# Elasticsearch Client
-#sudo pip install elasticsearch
-
-# MySQL Python Connector
-#wget --quiet http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.0.4-1.el7.noarch.rpm
-#sudo rpm -ivh mysql-connector-python-2.0.4-1.el7.noarch.rpm
-
-# PRAW - Reddit client
-#sudo pip install praw
-
-
-# #########################################################
-# MITIE NLP
-cd /vagrant
-git clone https://github.com/mit-nlp/MITIE.git
-cd /vagrant/MITIE
-
-# download models
-make MITIE-models
-
-# build executables and libs
-make
-
-# build Java bindings
-cd mitielib/java
-mkdir build
-cd build
-cmake ..
-cmake --build . --config Release --target install
-
-# #########################################################
-#
-# NodeJs and common Libs
-#
-sudo yum -y install nodejs npm
-
-
 # #########################################################
 #
 # Docker
@@ -95,12 +29,3 @@ sudo yum -y install nodejs npm
 sudo yum -y install docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
-
-
-# #########################################################
-#
-# Scala
-#
-#cd /vagrant
-#wget --quiet http://downloads.typesafe.com/scala/2.11.5/scala-2.11.5.rpm
-#sudo rpm -ivh scala-2.11.5.rpm
