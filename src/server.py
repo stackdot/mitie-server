7fa8c141972f56f02d188b41ee72dfd9e428cfe9
David.Schroh <dschroh@uncharted.software>
2015-06-02 08:43:24 -0400

    move server_ip and port to command-line

--- src/server.py
+++ src/server.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
-import sys, os, json
+# -*- coding: utf-8 -*- 
+
+"""
+ * Copyright © 2015 Uncharted Software Inc.
+ *
+ * Property of Uncharted™, formerly Oculus Info Inc.
+ * http://uncharted.software/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+""" 
+
+import sys, os, json, web
+
 parent = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(parent + '/../MITIE/mitielib')
 from mitie import *
 
-import web
 urls = { '/ner', 'NerHandler'}
 
 print "\nloading NER model..."
@@ -12,9 +41,9 @@ ner = named_entity_extractor('../MITIE/MITIE-models/english/ner_model.dat')
 print "\nTags output by this NER model:", ner.get_possible_ner_tags()
 
 class MyApplication(web.application):
-    def run(self, port=8080, *middleware):
-        func = self.wsgifunc(*middleware)
-        return web.httpserver.runsimple(func, ('0.0.0.0', port))
+    def run(self, argv):
+        func = self.wsgifunc()
+        return web.httpserver.runsimple(func, (sys.argv[1], int(sys.argv[2])))
 
 app = MyApplication(urls, globals())
 
@@ -60,4 +89,4 @@ class NerHandler:
 
 
 if __name__ == "__main__":
-    app.run(port=8888)
+    app.run(argv=sys.argv)
